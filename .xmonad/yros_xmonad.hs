e7e008169c48b19d0f25736fbde7ca1df4d248c6
Yros <yros.tw@gmail.com>
2021-08-16 14:45:10 +0200

    Init

---
+++ .xmonad/yros_xmonad.hs
@@ -0,0 +1,10 @@
+-- XMonad Yros Version --
+
+-- IMPORTS --
+	-- CORE --
+import XMonad
+import Data.Monoid
+import System.Exit
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
